96874e7b42611a85a85cfe0344b93d765aea5266
Simon <simonhermansen5dk@gmail.com>
2022-06-09 17:05:46 +0200

    Working lamp and peltier but not tested

---
+++ cooler.py
@@ -0,0 +1,29 @@
+from machine import Pin
+
+class Cooler:
+    def __init__(self, peltier_pin, fan_pin):
+        self.peltier = Pin(peltier_pin, Pin.OUT)
+        self.fan = Pin(fan_pin, Pin.OUT)
+        
+    def peltier_on(self):
+        self.peltier.on()
+        
+    def peltier_off(self):
+        self.peltier.off()
+        
+    def fan_on(self):
+        self.fan.on()
+        
+    def fan_off(self):
+        self.fan.off()
+        
+
+fan = Pin(12, Pin.OUT)
+
+peltier = Pin(27, Pin.OUT)
+
+
+peltier.off()
+fan.off()
+
+cooler = Cooler(peltier_pin = 27, Pin.OUT, fan_pin = 12)
--- stepper_motor.py
+++ stepper_motor.py
@@ -7,16 +7,16 @@ class StepperMotor:
         self.dir = Pin(dir_pin, Pin.OUT)
 
     def rotate(self, rotations, direction):
-        if direction == 'cw':
+        if direction == 'cw':   # clockwise
             self.dir.on()
-            for _ in range(rotations * 4000):
+            for _ in range(rotations * 1600):   # 1600 for eigth-step
                 self.step.on()
                 utime.sleep_us(500)
                 self.step.off()
                 utime.sleep_us(500)
-        elif direction == 'ccw':
+        elif direction == 'ccw':   # counter-clockwise
             self.dir.off()
-            for _ in range(rotations * 800):
+            for _ in range(rotations * 1600):
                 self.step.on()
                 utime.sleep_us(500)
                 self.step.off()
