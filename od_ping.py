7f3d7c00e3643ce644113224c163f9a9dc44409f
Nina Peuker <ninapeuker@ninas-air.students.clients.local>
2022-06-09 15:15:45 +0200

    OD sensor ADC adjusted and converted to class - not tested yet

--- od_ping.py
+++ od_ping.py
@@ -1,13 +1,33 @@
 import machine 
+from machine import ADC
 import time
 
-od = machine.Pin(32, machine.Pin.IN)
-adc = machine.ADC(od)        # create an ADC object acting on a pin
+class Photosensor:
+    def __init__(self,pin):
+        self.od = machine.Pin(pin, machine.Pin.IN)
+        self.adc = machine.ADC(self.od)    
+        self.adc.atten(ADC.ATTN_11DB)
+    
+    def readLoop(self,interval):
+        while True:
+            val_ana = self.adc.read_u16()     # read a raw analog value in the range 0-65535
+            val = self.adc.read()
+            # val_mv = adc.read_uv()      # read an analog value in microvolts
+            print("analogue: {}, simple read: {}".format(val_ana,val))
+            time.sleep(interval)
+
+    def read(self):
+        return adc.read()
+
+# od = machine.Pin(32, machine.Pin.IN)
+# adc = machine.ADC(od)        # create an ADC object acting on a pin
+
+# adc.atten(ADC.ATTN_11DB)
 
 
-
-while True:
-    val_ana = adc.read_u16()     # read a raw analog value in the range 0-65535
-    # val_mv = adc.read_uv()      # read an analog value in microvolts
-    print("analogue: {}".format(val_ana))
-    time.sleep(1)
+# while True:
+#     val_ana = adc.read_u16()     # read a raw analog value in the range 0-65535
+#     val = adc.read()
+#     # val_mv = adc.read_uv()      # read an analog value in microvolts
+#     print("analogue: {}, simple read: {}".format(val_ana,val))
+#     time.sleep(0.5)
