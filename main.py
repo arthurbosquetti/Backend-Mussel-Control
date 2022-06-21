c28ec68442d84780d64a34ed379647132c95ea33
Arthur Bosquetti <arthur@arthurs-air.students.clients.local>
2022-06-21 12:50:15 +0200

    added OD to web server

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 
 import utime
-# from umqtt.robust import MQTTClient
 
 from stepper_motor import StepperMotor
 from IOConnect import IOConnect
 from read_temp import Thermistor
 from lamp import Lamp
-import random
+from od_ping import Photosensor
 
 # import network
-import os
-import gc
-import sys
+# import os
+# import gc
+# import sys
 
 print("Sleeping for 5 sec...")
 utime.sleep(5)
@@ -20,7 +19,9 @@ mussel_pump = StepperMotor(step_pin = 14)
 mussel_pump.start_motor(5000)
 temp_sesnor = Thermistor(TEMP_SENS_ADC_PIN_NO = 32)
 lamp = Lamp(cool_pin = 25,warm_pin = 26)
-algae_pump =  StepperMotor(21)
+algae_pump =  StepperMotor(step_pin = 21)
+photosensor = Photosensor(pin = 34)
+
 
 # When to start feeding??
 # algae_pump.start_motor(5000)
@@ -46,20 +47,24 @@ def cb(topic, msg):
 
 ADAFRUIT_IO_URL = b'io.adafruit.com' 
 ADAFRUIT_USERNAME = b'arthurbosquetti'
-ADAFRUIT_IO_KEY = b'aio_aqKS72blBHkwOt0yNRO22jbMeJ6e'
+ADAFRUIT_IO_KEY = b'aio_adfn164R7iKFGqONzebrASLsCUdo'
 
-ADAFRUIT_IO_FEEDNAME1 = b'Mussel Pump'      # subscribe
-ADAFRUIT_IO_FEEDNAME2 = b'Temperature'      # subscribe, publish
-ADAFRUIT_IO_FEEDNAME3 = b'Algae Lamp'       # subscribe
-ADAFRUIT_IO_FEEDNAME4 = b'Algae Pump'       # subscribe, publish
+ADAFRUIT_IO_FEEDNAME1 = b'Mussel Pump'           # subscribe
+ADAFRUIT_IO_FEEDNAME2 = b'Temperature'           # subscribe, publish
+ADAFRUIT_IO_FEEDNAME3 = b'Algae Lamp'            # subscribe
+ADAFRUIT_IO_FEEDNAME4 = b'Algae Pump'            # subscribe, publish
+ADAFRUIT_IO_FEEDNAME5 = b'Algae Concentration'   # subscribe, publish
+
 
 mqtt_feedname1 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME1), 'utf-8')
 mqtt_feedname2 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME2), 'utf-8')
 mqtt_feedname3 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME3), 'utf-8')
 mqtt_feedname4 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME4), 'utf-8')
+mqtt_feedname5 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME5), 'utf-8')
 
-subscription_feeds = [mqtt_feedname1, mqtt_feedname2, mqtt_feedname3, mqtt_feedname4]
-publish_feeds      = [mqtt_feedname2, mqtt_feedname4]
+subscription_feeds = [mqtt_feedname1, mqtt_feedname2, mqtt_feedname3, 
+                      mqtt_feedname4, mqtt_feedname5]
+publish_feeds      = [mqtt_feedname2, mqtt_feedname4, mqtt_feedname5]
 
 IOManager = IOConnect(subscription_feeds, publish_feeds)
 IOManager.initClient(ADAFRUIT_IO_URL, ADAFRUIT_USERNAME, ADAFRUIT_IO_KEY, cb)
@@ -82,13 +87,14 @@ while True:
     if accum_time >= publish_time:
         
         temperature_data = temp_sesnor.read_temp()
+        algae_concentration = photosensor.algaeConcentration()
 
         print("Saving data locally...")
         data_file = open("data_file.txt", "a")
-        data_file.write("{}, {} \n".format(temperature_data, mussel_pump.freq))
+        data_file.write("{}, {} \n".format(temperature_data, mussel_pump.freq, algae_concentration))
         data_file.close()
 
-        data = [temperature_data, algae_pump.pwm.duty()]
+        data = [temperature_data, algae_pump.pwm.duty(), algae_concentration]
         IOManager.publishData(data)
         accum_time = 0
     IOManager.checkMessages()
