5ee7842d7d4d853d6e9712fa1e4259b71210e71d
Nina Peuker <ninapeuker@ninas-air.students.clients.local>
2022-06-21 12:05:41 +0200

    PID control can now be run once

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from stepper_motor import StepperMotor
 from IOConnect import IOConnect
 from read_temp import Thermistor
 from lamp import Lamp
+from PID_Thermistor import PID
 import random
 
 # import network
 import os
 import gc
 import sys
+
 
 print("Sleeping for 5 sec...")
 utime.sleep(5)
@@ -21,6 +23,7 @@ mussel_pump.start_motor(5000)
 temp_sesnor = Thermistor(TEMP_SENS_ADC_PIN_NO = 32)
 lamp = Lamp(cool_pin = 25,warm_pin = 26)
 algae_pump =  StepperMotor(21)
+
 
 # When to start feeding??
 # algae_pump.start_motor(5000)
@@ -44,25 +47,38 @@ def cb(topic, msg):
         elif msg.decode() == "3":
             lamp.cool_on()
 
-ADAFRUIT_IO_URL = b'io.adafruit.com' 
-ADAFRUIT_USERNAME = b'arthurbosquetti'
-ADAFRUIT_IO_KEY = b'aio_aqKS72blBHkwOt0yNRO22jbMeJ6e'
+ADAFRUIT_IO_URL = b'io.adafruit.com'
 
-ADAFRUIT_IO_FEEDNAME1 = b'Mussel Pump'      # subscribe
-ADAFRUIT_IO_FEEDNAME2 = b'Temperature'      # subscribe, publish
-ADAFRUIT_IO_FEEDNAME3 = b'Algae Lamp'       # subscribe
-ADAFRUIT_IO_FEEDNAME4 = b'Algae Pump'       # subscribe, publish
+#account details for logging mussel and algae information 
+ADAFRUIT_USERNAME1 = b'arthurbosquetti'
+ADAFRUIT_IO_KEY1 = b'aio_aqKS72blBHkwOt0yNRO22jbMeJ6e'
 
-mqtt_feedname1 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME1), 'utf-8')
-mqtt_feedname2 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME2), 'utf-8')
-mqtt_feedname3 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME3), 'utf-8')
-mqtt_feedname4 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME, ADAFRUIT_IO_FEEDNAME4), 'utf-8')
+#accont details for tracking the pid
+ADAFRUIT_USERNAME2 = b'niner'
+ADAFRUIT_IO_KEY2 = b'aio_ysPw12sGYdFXAC0jGFejtbA2h0w9'
+
+
+
+#feednames for logging mussel and algae information 
+ADAFRUIT_IO_FEEDNAME1_1 = b'Mussel Pump'      # subscribe
+ADAFRUIT_IO_FEEDNAME1_2 = b'Temperature'      # subscribe, publish
+ADAFRUIT_IO_FEEDNAME1_3 = b'Algae Lamp'       # subscribe
+ADAFRUIT_IO_FEEDNAME1_4 = b'Algae Pump'       # subscribe, publish
+
+#feednames for tracking pid
+
+
+
+mqtt_feedname1 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME1, ADAFRUIT_IO_FEEDNAME1_1), 'utf-8')
+mqtt_feedname2 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME1, ADAFRUIT_IO_FEEDNAME1_2), 'utf-8')
+mqtt_feedname3 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME1, ADAFRUIT_IO_FEEDNAME1_3), 'utf-8')
+mqtt_feedname4 = bytes('{:s}/feeds/{:s}'.format(ADAFRUIT_USERNAME1, ADAFRUIT_IO_FEEDNAME1_4), 'utf-8')
 
 subscription_feeds = [mqtt_feedname1, mqtt_feedname2, mqtt_feedname3, mqtt_feedname4]
 publish_feeds      = [mqtt_feedname2, mqtt_feedname4]
 
 IOManager = IOConnect(subscription_feeds, publish_feeds)
-IOManager.initClient(ADAFRUIT_IO_URL, ADAFRUIT_USERNAME, ADAFRUIT_IO_KEY, cb)
+IOManager.initClient(ADAFRUIT_IO_URL, ADAFRUIT_USERNAME1, ADAFRUIT_IO_KEY1, cb)
 IOManager.setWifi("Arthur iPhone", "aviaodepapel")
 IOManager.connectWifi(10, 3)
 IOManager.clientConnectSubscribe()
